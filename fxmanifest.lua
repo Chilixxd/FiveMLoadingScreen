f4e4c9dbe39359c9445552ffa9f217e046164ff0
Chilixxd <59507848+Chilixxd@users.noreply.github.com>
2021-06-14 21:19:53 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'chili'
+description 'Discord : https://discord.gg/swNHWtVnP6'
+version '2.0'
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+files {
+    'index.html',
+    'css/index.css',
+    'js/strings.js',
+    'js/typed.min.js',
+    'music/music.mp3',
+    'video/lls1.mp4'
+}
